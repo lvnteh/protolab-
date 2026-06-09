e07daeff36e0c92899a5c04af5ddda4788bbbebb
lhernadi <82042741+lhernadi@users.noreply.github.com>
2026-06-09 23:57:57 +0200

    feat: add injectPreview to inject service

--- src/services/inject.js
+++ src/services/inject.js
@@ -1,25 +1,27 @@
-// src/services/inject.js
-const SUGAR_CSS = `<link rel="stylesheet" href="https://client-version.cf.emarsys.net/ui/latest/css/app.css">`;
-const SUGAR_JS  = `<script src="https://client-version.cf.emarsys.net/ui/latest/js/app.js"></script>`;
-
 function sdkScript(protoId, email) {
   return `<script src="/sdk/feedback.js" data-proto-id="${protoId}" data-email="${encodeURIComponent(email)}"></script>`;
 }
 
-function injectSdk(html, protoId, email) {
-  const headInjection = `\n${SUGAR_CSS}\n${SUGAR_JS}\n`;
-  const bodyInjection = `\n${sdkScript(protoId, email)}\n`;
-
-  let result = html;
-  if (result.includes('</head>')) {
-    result = result.replace('</head>', `${headInjection}</head>`);
-  }
-  if (result.includes('</body>')) {
-    result = result.replace('</body>', `${bodyInjection}</body>`);
-  } else {
-    result += bodyInjection;
-  }
-  return result;
+function previewScript(protoId, highlightId, commentsJson) {
+  const safeComments = commentsJson.replace(/</g, '\\u003c').replace(/"/g, '&quot;');
+  return `<script src="/sdk/preview.js" data-proto-id="${protoId}" data-highlight-comment="${highlightId}" data-comments="${safeComments}"></script>`;
 }
 
-module.exports = { injectSdk };
+function injectBefore(html, scriptTag) {
+  const lastBody = html.lastIndexOf('</body>');
+  const injection = `\n${scriptTag}\n`;
+  if (lastBody !== -1) {
+    return html.slice(0, lastBody) + injection + html.slice(lastBody);
+  }
+  return html + injection;
+}
+
+function injectSdk(html, protoId, email) {
+  return injectBefore(html, sdkScript(protoId, email));
+}
+
+function injectPreview(html, protoId, highlightId, commentsJson) {
+  return injectBefore(html, previewScript(protoId, highlightId, commentsJson));
+}
+
+module.exports = { injectSdk, injectPreview };
